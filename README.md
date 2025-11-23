03301b5768f8b7cdfe1bb3542f02cae7c8e46a69
Michelle Domingues <michelle08099410@gmail.com>
2025-11-22 23:11:37 -0300

    feat: adicionado index.js, readme  e atribuido a funcionalidade do campo input e botão

---
+++ README.md
@@ -0,0 +1,7 @@
+ - Funcionalidades que o projeto precisa ter: 
+
+ - buscar usuários do github pelo nome de usuário
+ - definir informações do perfil : Avatar, nome, bio, seguidores, seguindo.
+ - Listar ultimos 10 repositórios com estatisticas (stars, forks, watchers, linguagem)
+ - tratar estados de loading: Erro e usuário não encontrado.
+ - Interface responsiva e animações suaves.
---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+//Tornando input e button funcionais:
+
+const inputSearch = document.getElementById ('input-search');
+const btnSearch = document.getElementById ('btn-search');
+
+btnSearch.addEventListener ('click', () => {
+
+    const searchTerm = inputSearch.value;
+
+    if (searchTerm) {
+
+        console.log ('O valor digitado é:', searchTerm);
+
+    } else {
+
+        alert('Por favor, digite um nome de usuário.');
+    }
+
+});
+
+
