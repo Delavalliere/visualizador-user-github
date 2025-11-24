7b026b5fa1fcf4ce2500bf9d001c32e32255b92e
Michelle Domingues <michelle08099410@gmail.com>
2025-11-24 15:13:09 -0300

    Feat: Adiciona foto de perfil, nome de usuário, bio e estado de carregamento.

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,58 @@
 //Tornando input e button funcionais:
 
-const inputSearch = document.getElementById ('input-search');
-const btnSearch = document.getElementById ('btn-search');
+const baseUrl = "https://api.github.com/users/";
 
-btnSearch.addEventListener ('click', () => {
+const inputSearch = document.getElementById('input-search');
+const btnSearch = document.getElementById('btn-search');
+const profileResults = document.querySelector('.profile-results');
+
+btnSearch.addEventListener('click', async () => {
 
     const searchTerm = inputSearch.value;
 
     if (searchTerm) {
+        profileResults.innerHTML = '<p>Carregando...</p>'; // Loading message
 
-        console.log ('O valor digitado é:', searchTerm);
+        try {
 
+            const response = await fetch(`${baseUrl}${searchTerm}`);
+
+            if (!response.ok) {
+                profileResults.innerHTML = ''; // Clear loading message
+                alert('Usuário não encontrado, verifique o nome do usuário e tente novamente.');
+                return;
+            }
+
+            const Userdata = await response.json();
+            console.log(Userdata); // Apenas para verificar se os dados estão sendo buscados corretamente.
+            profileResults.innerHTML = 
+            ` <div class="profile-card">
+
+                <img src="${Userdata.avatar_url}" alt="Avatar de ${Userdata.login}" class="profile-avatar">
+                <div> 
+                    <h2>${Userdata.name}</h2>
+                    <p>${Userdata.bio || 'Não possui bio cadastrada 😢.'}</p>
+                </div>
+            </div> ` 
+
+        } catch (error) {
+            profileResults.innerHTML = ''; // Clear loading message
+            console.error('Erro ao buscar perfil do usuário:', error);
+            alert('Ocorreu um erro ao buscar o perfil do usuário. Por favor, tente novamente mais tarde.');
+        }
     } else {
 
-        alert('Por favor, digite um nome de usuário.');
-    }
+        alert('Por favor, insira um nome de usuário do GitHub.');
 
+    }
 });
+
+
+
+
+
+
+
+
 
 
